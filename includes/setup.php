1d34181b745c3f7b5df309ef6088c8c35e402429
jswebschmiede <joergschoeneburg@freenet.de>
2022-04-11 13:15:37 +0200

    textdomain

--- includes/setup.php
+++ includes/setup.php
@@ -2,11 +2,13 @@
 
 function your_tpl_setup_theme()
 {
-    // Add theme support for HTML5 Semantic Markup
     add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_theme_support('custom-logo');
+    add_theme_support('custom-header');
     add_theme_support('woocommerce');
-    register_nav_menu('main-menu', __('Primary Menu', 'your_tpl'));
+    add_theme_support('responsive-embeds');
+    add_theme_support('editor-styles');
+    load_theme_textdomain('your_tpl', get_template_directory() . '/language');
 }
