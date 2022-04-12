03d7f8acf8e0e0d2a5cf5e5298717c5cdfab4019
jswebschmiede <joergschoeneburg@freenet.de>
2022-04-12 13:34:34 +0200

    cleanup

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,66 +1,9 @@
 // Webpack Imports
 import * as bootstrap from "bootstrap";
-import AOS from "aos";
-import Swiper from "swiper/bundle";
-import $ from "jquery";
 import "bs5-lightbox";
-
-window.jQuery = $;
-window.$ = $;
-
-function isMobile(width) {
-  var windowWidth = window.innerWidth;
-  var ismobile = window.innerWidth <= width ? true : false;
-  return ismobile;
-}
-
-var swiper = new Swiper(".timeline", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  freeMode: false,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 40,
-    },
-
-    780: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  },
-  mousewheel: false,
-});
 
 (function () {
   "use strict";
-
-  AOS.init({
-    duration: 950,
-  });
-
-  $(".open-menu,.close").on("click", function (ev) {
-    if ($(".menu-overlay").hasClass("open")) {
-      $(".menu-overlay").removeClass("open");
-    } else {
-      $(".menu-overlay").addClass("open");
-    }
-
-    ev.preventDefault();
-  });
 
   // Focus input if Searchform is empty
   [].forEach.call(document.querySelectorAll(".search-form"), (el) => {
@@ -81,44 +24,3 @@ var swiper = new Swiper(".timeline", {
   }
   hidePreloader();
 })();
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-  setSticky();
-  setShrink();
-};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-var navbarShrink = document.getElementById("navbar__shrink");
-
-if (navbar) {
-  // Get the offset position of the navbar
-  var sticky = navbar.offsetTop;
-}
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function setSticky() {
-  if (navbar) {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
-    } else {
-      navbar.classList.remove("sticky");
-    }
-  }
-}
-
-function setShrink() {
-  if (!isMobile(768)) {
-    if (navbarShrink) {
-      if (window.pageYOffset >= 1) {
-        navbarShrink.classList.add("shrink");
-        $(".wrapper").css("margin-top", 0);
-      } else {
-        navbarShrink.classList.remove("shrink");
-        $(".wrapper").css("margin-top", 120);
-      }
-    }
-  }
-}
-
--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -9,7 +9,6 @@ function your_tpl_enqueue()
     wp_register_style('your_tpl_app', $uri . '/assets/css/main.css', array(), $version, 'all');
     wp_register_script('your_tpl_app', $uri . '/assets/js/main.bundle.js', array('jquery'), $version, true);
 
-    wp_enqueue_script('jquery');
     wp_enqueue_style('your_tpl_google_fonts');
     wp_enqueue_style('your_tpl_app');
     wp_enqueue_script('your_tpl_app');
